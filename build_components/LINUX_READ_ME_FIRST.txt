ed44af78a4f09bcf4c4e5ed62249c12cd62caaab
Robert <rfhood@mail.usf.edu>
2020-02-03 01:24:33 -0500

    Added support to build games for Linux and Mac.

---
+++ build_components/LINUX_READ_ME_FIRST.txt
@@ -0,0 +1,9 @@
+Note for Linux users:
+Depending on your security dependencies, you may have to enable permissions for the Java components and the shell scripts to run.
+If you click on the launcher script and nothing happens, try this solution for the following files:
+
+LAUNCH_GAME.sh
+java/bin/java
+java/bin/jrunscript
+java/bin/keytool
+java/bin/TicTacToeFX
--- build_windows_app.bat
+++ build_windows_app.bat
@@ -14,14 +14,32 @@ set VERSION_NUMBER=1.0.0
 @REM Paths to folders which are a part of the build process.
 set JAR_PATH=out\artifacts\TicTacToeFX_jar
 set OUTPUT_DIR=%JAR_PATH%\build
-set MODULES_TARGET_DIR=%OUTPUT_DIR%\modules
-set GAME_FOLDER_DIR=%OUTPUT_DIR%\TicTacToeFX_Win%VERSION_NUMBER%
-set WIN_IMAGE_TARGET_DIR=%GAME_FOLDER_DIR%\java
 
-@REM Paths to JavaFX Jmod modules. *** SET FOR LOCAL DEV ENVIRONMENT BEFORE RUNNING***
+set WIN_MODULES_TARGET_DIR=%OUTPUT_DIR%\modules\win
+set WIN_GAME_FOLDER_DIR=%OUTPUT_DIR%\TicTacToeFX_Win_%VERSION_NUMBER%
+set WIN_IMAGE_TARGET_DIR=%WIN_GAME_FOLDER_DIR%\java
+
+set MAC_MODULES_TARGET_DIR=%OUTPUT_DIR%\modules\mac
+set MAC_GAME_FOLDER_DIR=%OUTPUT_DIR%\TicTacToeFX_Mac_%VERSION_NUMBER%
+set MAC_IMAGE_TARGET_DIR=%MAC_GAME_FOLDER_DIR%\java
+
+set LINUX_MODULES_TARGET_DIR=%OUTPUT_DIR%\modules\linux
+set LINUX_GAME_FOLDER_DIR=%OUTPUT_DIR%\TicTacToeFX_Linux_%VERSION_NUMBER%
+set LINUX_IMAGE_TARGET_DIR=%LINUX_GAME_FOLDER_DIR%\java
+
+
+
+@REM Paths to Java and JavaFX jmods. *** SET FOR LOCAL DEV ENVIRONMENT BEFORE RUNNING OR COMMENT OUT***
 set WINDOWS_JAVAFX_JMOD_PATH=D:\dev\libs\JavaFX_jmods\openjfx-13.0.2_windows-x64_bin-jmods\javafx-jmods-13.0.2
-@REM set LINUX_JAVAFX_JMOD_PATH =
-@REM set MAC_JAVAFX_JMOD_PATH =
+set LINUX_JAVAFX_JMOD_PATH=D:\dev\libs\JavaFX_jmods\openjfx-13.0.2_linux-x64_bin-jmods\javafx-jmods-13.0.2
+set MAC_JAVAFX_JMOD_PATH=D:\dev\libs\JavaFX_jmods\openjfx-13.0.2_osx-x64_bin-jmods\javafx-jmods-13.0.2
+
+@REM For windows, if the JDK is set up, then the path to the JDK jmods is implicit. Others must be set.
+@REM Comment out if not using.
+set MAC_JDK_JMOD_PATH=D:\dev\libs\jdk-13.0.2.jdk_mac\Contents\Home\jmods
+set LINUX_JDK_JMOD_PATH=D:\dev\libs\jdk-13.0.2_linux\jmods
+
+
 
 @echo ###############################################
 @echo ########    Beginning Build script.    ########
@@ -33,18 +51,39 @@ rmdir /Q /S %OUTPUT_DIR%
 
 @echo.
 @echo ########    Create needed folders    ########
-mkdir %MODULES_TARGET_DIR%
+mkdir %WIN_MODULES_TARGET_DIR%
+mkdir %MAC_MODULES_TARGET_DIR%
+mkdir %LINUX_MODULES_TARGET_DIR%
 
 
 @echo.
 @echo ########    Converting existing .jar file to module.    ########
-jmod  create  --class-path  %JAR_PATH%\TicTacToeFX.jar  --main-class edu.usf.mail.rfhood.TicTacToeFX.main.Main  %MODULES_TARGET_DIR%\TicTacToeFX.jmod
+@REM Comment out jmod creation for architectures we are not targeting.
+@REM Target architecture for windows:       windows-amd64
+@REM Target architecture for mac:           macos-amd64
+@REM Target architecture for linux:         linux-amd64
+
+jmod  create  --class-path  %JAR_PATH%\TicTacToeFX.jar  --main-class edu.usf.mail.rfhood.TicTacToeFX.main.Main  %WIN_MODULES_TARGET_DIR%\TicTacToeFX.jmod
+jmod  create  --target-platform macos-amd64  --class-path  %JAR_PATH%\TicTacToeFX.jar  --main-class edu.usf.mail.rfhood.TicTacToeFX.main.Main  %MAC_MODULES_TARGET_DIR%\TicTacToeFX.jmod
+jmod  create  --target-platform linux-amd64  --class-path  %JAR_PATH%\TicTacToeFX.jar  --main-class edu.usf.mail.rfhood.TicTacToeFX.main.Main  %LINUX_MODULES_TARGET_DIR%\TicTacToeFX.jmod
 
 
 @echo.
 @echo ########    Linking to JavaFX platform-specific jmods    ########
-jlink  --output %WIN_IMAGE_TARGET_DIR%  --module-path  %MODULES_TARGET_DIR%;%WINDOWS_JAVAFX_JMOD_PATH%  --add-modules  edu.usf.mail.rfhood.TicTacToeFX  --launcher TicTacToeFX=edu.usf.mail.rfhood.TicTacToeFX
+@REM Comment out jlinking for architectures we are not targeting.
+jlink  --output %WIN_IMAGE_TARGET_DIR%  --module-path  %WIN_MODULES_TARGET_DIR%;%WINDOWS_JAVAFX_JMOD_PATH%  --add-modules  edu.usf.mail.rfhood.TicTacToeFX  --launcher TicTacToeFX=edu.usf.mail.rfhood.TicTacToeFX
+jlink  --output %MAC_IMAGE_TARGET_DIR%  --module-path  %MAC_JDK_JMOD_PATH%;%MAC_MODULES_TARGET_DIR%;%MAC_JAVAFX_JMOD_PATH%  --add-modules  edu.usf.mail.rfhood.TicTacToeFX  --launcher TicTacToeFX=edu.usf.mail.rfhood.TicTacToeFX
+jlink  --output %LINUX_IMAGE_TARGET_DIR%  --module-path  %LINUX_JDK_JMOD_PATH%;%LINUX_MODULES_TARGET_DIR%;%LINUX_JAVAFX_JMOD_PATH%  --add-modules  edu.usf.mail.rfhood.TicTacToeFX  --launcher TicTacToeFX=edu.usf.mail.rfhood.TicTacToeFX
 
-@REM Also copy the launcher .bat file into the windows-specific image folder.
-copy  build_components\LAUNCH_GAME.bat  %GAME_FOLDER_DIR%
 
+@echo.
+@echo ########    Copying launcher files and misc. into image directories    ########
+@REM Also copy the launcher .bat or .sh file into the architecture-specific image folder.
+@REM Again, comment out any copies for architectures we are not targeting.
+copy  build_components\LAUNCH_GAME.bat  %WIN_GAME_FOLDER_DIR%
+copy  build_components\LAUNCH_GAME.sh   %MAC_GAME_FOLDER_DIR%
+copy  build_components\LAUNCH_GAME.sh   %LINUX_GAME_FOLDER_DIR%
+
+
+@REM Lastly add any other files such as notes, ReadMe files, game manuals, etc.
+copy  build_components\LINUX_READ_ME_FIRST.txt   %LINUX_GAME_FOLDER_DIR%
